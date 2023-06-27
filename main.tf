6470d119d67dd8c574d67b14f6072abea37a5fd0
subbum <subbu@gmail.com>
2023-06-27 12:25:46 +0100

    rabbitmg spot price change

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@
 
 resource "aws_spot_instance_request" "rabbitmq" {
   ami           = data.aws_ami.ami.id
-  spot_price    = "0.03"
   instance_type = var.instance_type
   subnet_id = var.subnet_ids[0]
   wait_for_fulfillment = true
