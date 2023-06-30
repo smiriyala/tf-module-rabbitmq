6a02026945568d81c4c8b136aa98793a96de3fc8
subbum <subbu@gmail.com>
2023-06-30 13:19:21 +0100

    setup rabbitmq separate instnace and Iam policy, DNS record and Ansible script executio added

--- main.tf
+++ main.tf
@@ -7,6 +7,15 @@ resource "aws_spot_instance_request" "rabbitmq" {
   subnet_id = var.subnet_ids[0]
   wait_for_fulfillment = true
 
+  vpc_security_group_ids = [aws_security_group.main.id]
+  iam_instance_profile   = aws_iam_instance_profile.main.name
+
+    # OPITON - B other approch is
+  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
+    component = var.component
+    env = var.env
+  } ))
+
   tags = merge(
     var.tags,
     { Name = "${var.env}-rabbitmq" }
@@ -18,4 +27,47 @@ resource "aws_ec2_tag" "rabitmqtag" {
     key = "Name"
     value = "${var.env}-rabbitmq"
   
+}
+
+resource "aws_route53_record" "main" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = "rabbitmq-${var.env}.${var.dns_domain}"
+  type    = "A"
+  ttl     = 30
+  records = [aws_spot_instance_request.rabbitmq.private_ip]
+}
+
+resource "aws_security_group" "main" {
+  name        = "rabbitmq-${var.env}"
+  description = "rabbitmq-${var.env}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "RABBITMQ"
+    from_port   = 5672
+    to_port     = 5672
+    protocol    = "tcp"
+    cidr_blocks = var.allow_subnets
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.bastion_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "rabbitmq-${var.env}" }
+  )
 }
--- vars.tf
+++ vars.tf
@@ -2,3 +2,11 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "tags" {}
 variable "instance_type" {}
+
+variable "allow_subnets" {}
+variable "vpc_id" {}
+variable "bastion_cidr" {}
+variable "dns_domain" {}
+variable "component" {
+  default = "rabbitmq"
+}
