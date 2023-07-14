3fb6089366efa24f1f5c143af4291e055d391e4d
subbum <subbu@gmail.com>
2023-07-14 14:31:18 +0100

    rabbitmq changed from spotinstnace

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #RabiitMQ Instance add as it doesnt support cluster /service implementation.
 # Hence we are going to add ec2 instance.
 
-resource "aws_spot_instance_request" "rabbitmq" {
+resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
   subnet_id = var.subnet_ids[0]
@@ -22,12 +22,12 @@ resource "aws_spot_instance_request" "rabbitmq" {
   )
 }
 
-resource "aws_ec2_tag" "rabitmqtag" {
+/* resource "aws_ec2_tag" "rabitmqtag" {
     resource_id = aws_spot_instance_request.rabbitmq.spot_instance_id
     key = "Name"
     value = "${var.env}-rabbitmq"
   
-}
+} */
 
 resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.domain.zone_id
